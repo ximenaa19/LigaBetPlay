25e9a52e3c5d20e1fbaf07ef08e60b5292706f5f
karen ximena afanador suarez <222234207+ximenaa19@users.noreply.github.com>
2026-03-24 23:06:36 -0500

    feat: :sparkles: Clase Equipos

---
+++ LigaBetPlay/utils/Equipos.cs
@@ -0,0 +1,56 @@
+using System;
+
+namespace LigaBetPlay.utils;
+
+public class Equipos
+{
+    public string Nombre { get; set;}
+    public int PJ { get; set;}
+    public int PG { get; set;}
+    public int PE { get; set;}
+    public int PP { get; set;}
+    public int GF { get; set;}
+    public int GC { get; set;}
+    public int TP => (PG * 3) + (PE * 1);
+
+// Constructor
+    public Equipos(string nombre)
+        {
+            Nombre = nombre;
+            PJ = 0;
+            PG = 0;
+            PE = 0;
+            PP = 0;
+            GF = 0;
+            GC = 0;
+        }
+    
+// Metodo para registrar resultados
+    public void RegistrarResultado(int golesAFavor, int golesEnContra)
+    {
+        PJ++;
+        GF += golesAFavor;
+        GC += golesEnContra;
+
+        if (golesAFavor > golesEnContra)
+        {
+            PG++;
+        }
+        else if (golesAFavor == golesEnContra)
+        {
+            PE++;
+        }
+        else
+        {
+            PP++;
+        }
+    }
+
+    public override string ToString()
+        {
+            return $"{Nombre,-22} | PJ:{PJ,2} | PG:{PG,2} | PE:{PE,2} | PP:{PP,2} | GF:{GF,3} | GC:{GC,3} | Pts:{TP,3}";
+        }
+
+
+
+}
---
+++ LigaBetPlay/utils/Menu.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace LigaBetPlay.utils;
+
+public class Menu
+{
+
+}
