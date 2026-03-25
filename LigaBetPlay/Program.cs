c48d83e58b8b6b9e4240865862e5c44d924bcd84
karen ximena afanador suarez <222234207+ximenaa19@users.noreply.github.com>
2026-03-24 23:51:22 -0500

    feat: :sparkles: Main

--- LigaBetPlay/Program.cs
+++ LigaBetPlay/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+using LigaBetPlay.utils;
+
+namespace LigaBetPlay;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Menu menu = new Menu();
+        menu.Iniciar();
+    }
+}
